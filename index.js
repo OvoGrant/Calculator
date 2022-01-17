f646a0f96e72361f08064bd0db37c865cf40d6a6
Ogheneovo Grant-Oyeye <ogheneovo.grant-oyeye@mail.mcgill.ca>
2022-01-16 20:29:50 -0500

    beganBasicFunctionality

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const buttonList = document.querySelectorAll(".CalcButton");
-var num = 0
+const animation = document.querySelectorAll('.animation')[0]
+const operands = document.getElementsByClassName('operands')[0]
+const equalsButton = document.getElementById('Equals');
+let x = 0;
 
+
+const addBlock = (event) => {
+    let newBlock = document.createElement('div');
+    newBlock.innerHTML = event.target.innerHTML;
+    newBlock.id = "stack" + x;
+    x++;
+    animation.appendChild(newBlock)
+}
 
 buttonList.forEach(button => {
-    button.addEventListener('click', event => {
-
-    //add the function necessary for the event function
-
-    });
+    button.addEventListener('click' , addBlock);
 })
+
+
+
+
+
